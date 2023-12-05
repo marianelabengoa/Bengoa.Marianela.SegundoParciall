37261035dcd31648a14073bbd6566b7fb3b8a4ba
svtcliffe <vcabelloherrera@gmail.com>
2023-12-05 17:45:04 -0300

    areglo generics

--- Entidades/GenericsListaEspera.cs
+++ Entidades/GenericsListaEspera.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class GenericsListaEspera<Visita> //where Visita : class, new()
+    public class GenericsListaEspera<T> where T : Visita
     {
         private static List<Visita> lista;
         private int capacidad;
